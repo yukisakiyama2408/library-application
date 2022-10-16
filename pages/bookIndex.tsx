88d482be9274cc6e6b6c74f433d15444bf22b750
Yuki Sakiyama <yuki.king.0422@gmail.comyuki2408>
2022-10-16 17:16:54 +0900

    getStaticPropsコンポーネントを追加

--- pages/bookIndex.tsx
+++ pages/bookIndex.tsx
@@ -1,16 +1,38 @@
 import {useState,useEffect} from 'react'
 import axios from "axios"
+import { gql } from '@apollo/client';
+import client from './api/apollo-client';
 
-// interface Books{
-//    index: { id: string; title: string; author:string,image_url:string }[]
-// }
 interface Book{
-      id: string; title: string; author:string,image_url:string 
-  }
+  id: string; title: string; author:string,image_url:string 
+}
+
+export async function getStaticProps() {
+  const { data } = await client.query({
+    query: gql`
+    query{
+      books_table{
+        id
+        title
+        author
+        image_url
+      }
+    }
+    `,
+  });
+
+  return {
+    props: {
+      books: data.books_table,
+    },
+ };
+}
+
   
 
-const BookIndex = ()=> {
-  const [books, setBooks] = useState<Array<Book>>([]);
+const BookIndex= ({books})=> {
+  console.log(books)
+  // const [books, setBooks] = useState<Array<Book>>([]);
   // useEffect(() => {
   //   (async()=>{
   //     const { data } = await client.query({
@@ -34,25 +56,25 @@ const BookIndex = ()=> {
   //   });
   // },[])
 
-  useEffect(() => {
-    axios
-    .get('api/hello', {
-      headers: {
-        "Content-Type": "application/json",
-      },
-      // data: {},
-    })
-    .then((res) => {
-     console.log(res.data.data.books_table)
-     console.log(res.data.data)
-     console.log(res.data.data.data)
-      setBooks(res.data.data.data.books_table);
-    });
-  },[])
+  // useEffect(() => {
+  //   axios
+  //   .get('api/hello', {
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //     // data: {},
+  //   })
+  //   .then((res) => {
+  //    console.log(res.data.data.books_table)
+  //    console.log(res.data.data)
+  //    console.log(res.data.data.data)
+  //     setBooks(res.data.data.data.books_table);
+  //   });
+  // },[])
 
   return (
     <div>
-        {books.map((book) => (
+        {books.map((book:any) => (
           <div key={book.id}>
           <p >{book.title}</p>
           <img src={book.image_url}/>
