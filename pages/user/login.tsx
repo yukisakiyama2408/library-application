f25122fc68e11bfedd4923b5ed8f57dc227637ff
Yuki Sakiyama <yuki.king.0422@gmail.comyuki2408>
2022-12-01 18:20:13 +0900

    ログインコンポーネント修正

--- pages/user/login.tsx
+++ pages/user/login.tsx
@@ -1,19 +1,6 @@
-import { Auth0Client } from "@auth0/auth0-spa-js";
 
-const auth0 = new Auth0Client({
-    domain: 'YOUR_DOMAIN',
-    clientId: 'YOUR_CLIENT_ID'
-  });
-  
-//   function (user, context, callback) {
-//     const namespace = "https://hasura.io/jwt/claims";
-//     context.accessToken[namespace] =
-//       {
-//         'x-hasura-default-role': 'user',
-//         // do some custom logic to decide allowed roles
-//         'x-hasura-allowed-roles': ['user'],
-//         'x-hasura-user-id': user.user_id
-//       };
-//     callback(null, user, context);
-//   }
+const Login=()=>{
+    return<>aaa</>
+}
 
+export default Login
