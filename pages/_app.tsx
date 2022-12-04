ac118fd4a1a153e692a5866ccaa790e1b7adc22b
Yuki Sakiyama <yuki.king.0422@gmail.comyuki2408>
2022-12-04 11:23:31 +0900

    auth0の修正

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -17,10 +17,10 @@ interface MyAppProps extends AppProps {
 function MyApp({ Component, emotionCache = clientSideEmotionCache, pageProps }: AppProps) {
   return (
     <Auth0Provider
-    domain={process.env.NEXT_PUBLIC_AUTH0_DOMAIN||""}
-    clientId={process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID||""}
-    redirectUri={`${process.env.NEXT_PUBLIC_APP_URL}/`}
-    >
+    domain="dev-95au-j9j.us.auth0.com"
+    clientId="je55u4Ols3LCJTZaKjPCdcgt1Xzh0ALN"
+    redirectUri="http://localhost:3000"
+        >
        <CacheProvider value={emotionCache}>
        <Head>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
--- pages/user/login.tsx
+++ pages/user/login.tsx
@@ -2,10 +2,18 @@ import { useAuth0 } from "@auth0/auth0-react"
 
 
 const Login=()=>{
-    const{loginWithRedirect}=useAuth0()
+    const{loginWithRedirect,logout,isAuthenticated}=useAuth0()
+    const handleLogout =()=>{
+        logout()
+    }
     return(
     <>
-    <button onClick={loginWithRedirect}>ログイン</button>
+    <div>
+        {isAuthenticated ?
+            <button onClick={handleLogout}>ログアウト</button>
+            :  <button onClick={loginWithRedirect}>ログイン</button>
+        }
+    </div>
     </>)
 }
 
