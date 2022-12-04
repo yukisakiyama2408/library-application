5d2e449a9c8b376512110b7b2dfdff8e9ef8f024
Yuki Sakiyama <yuki.king.0422@gmail.comyuki2408>
2022-12-04 11:53:16 +0900

    hasuraとauth0を接続

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -20,6 +20,7 @@ function MyApp({ Component, emotionCache = clientSideEmotionCache, pageProps }: 
     domain="dev-95au-j9j.us.auth0.com"
     clientId="je55u4Ols3LCJTZaKjPCdcgt1Xzh0ALN"
     redirectUri="http://localhost:3000"
+    audience="http://localhost:8080/v1/graphql"
         >
        <CacheProvider value={emotionCache}>
        <Head>
