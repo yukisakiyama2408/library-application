620d1e7de12ed66b424efdc8eadc0abbb55ce2b5
Yuki Sakiyama <yuki.king.0422@gmail.com>
2022-12-24 10:59:32 +0900

    本アプリをビルド

--- pages/book/[id].tsx
+++ pages/book/[id].tsx
@@ -1,12 +1,12 @@
-import { gql, useQuery} from '@apollo/client';
-import { useRouter } from 'next/router';
-import Link from 'next/link';
-import BookDelete from './[id]/book-delete';
-import { Container,Typography,Button,Box } from '@mui/material';
+import { gql, useQuery } from "@apollo/client";
+import { useRouter } from "next/router";
+import Link from "next/link";
+import BookDelete from "./[id]/book-delete";
+import { Container, Typography, Button, Box } from "@mui/material";
 
 const GET_BOOK = gql`
-query getBook($id:Int!) {
-    books_table(where:{id:{_eq:$id}}){
+  query getBook($id: Int!) {
+    books_table(where: { id: { _eq: $id } }) {
       id
       title
       author
@@ -14,51 +14,60 @@ query getBook($id:Int!) {
       description
     }
   }
-`
+`;
 
-interface Book{
-    id: number; title: string; author:string,image_url:string,description:string
-  }
+interface Book {
+  id: number;
+  title: string;
+  author: string;
+  image_url: string;
+  description: string;
+}
 
-const BookDetail =()=>{
-    const router = useRouter();
-    const id = router.query.id
-    const {data} = useQuery(GET_BOOK,{
-        variables:{id}
-    })
-    console.log(data)
-    const book:Book= !data? []:data.books_table[0]
-     console.log(book)
+const BookDetail = () => {
+  const router = useRouter();
+  const id = router.query.id;
+  const { data } = useQuery(GET_BOOK, {
+    variables: { id },
+  });
+  console.log(data);
+  const book: Book = !data ? [] : data.books_table[0];
+  console.log(book);
 
-    return( 
+  return (
     <div>
-       <div>
+      <div>
         <Button variant="contained" href="/book/book-index">
           Back to Home
         </Button>
       </div>
       <Container>
-      <div className='image-section'>
-          <img src={book.image_url}/>
-          </div>
-          <div className='info-section'>
+        <div className="image-section">
+          <img src={book.image_url} alt="book cover image" />
+        </div>
+        <div className="info-section">
           <h2> {book.title}</h2>
           <p>{book.author}</p>
-          </div>
+        </div>
         <p>{book.description}</p>
       </Container>
       <div>
         <div>
-      <Button variant="contained">
-      <Link href="/book/[id]/book-update" as={`/book/${book.id}/book-update`} >
-        Update
-      </Link>
-      </Button>
+          <Button variant="contained">
+            <Link
+              href="/book/[id]/book-update"
+              as={`/book/${book.id}/book-update`}
+            >
+              Update
+            </Link>
+          </Button>
+        </div>
+        <div>
+          <BookDelete id={book.id} />
+        </div>
       </div>
-      <div><BookDelete id={book.id}/></div>
-      </div>
-      
-    </div>)
-}
+    </div>
+  );
+};
 
-export default BookDetail
+export default BookDetail;
--- pages/book/book-index.tsx
+++ pages/book/book-index.tsx
@@ -1,73 +1,90 @@
-import { gql, useQuery} from '@apollo/client';
-import GlobalHeader from '../globalHeader';
-import Link from 'next/link';
-import { Card ,CardContent,CardActionArea,CardMedia,Typography,Container,Button,Grid} from '@mui/material';
-
+import { gql, useQuery } from "@apollo/client";
+import GlobalHeader from "../globalHeader";
+import Link from "next/link";
+import {
+  Card,
+  CardContent,
+  CardActionArea,
+  CardMedia,
+  Typography,
+  Container,
+  Button,
+  Grid,
+} from "@mui/material";
 
 const GET_BOOKS = gql`
-query GetBooks {
-  books_table{
-    id
-    title
-    author
-    image_url
+  query GetBooks {
+    books_table {
+      id
+      title
+      author
+      image_url
+    }
   }
-}
-`
+`;
 
-interface Book{
-  id: string; title: string; author:string,image_url:string 
+interface Book {
+  id: string;
+  title: string;
+  author: string;
+  image_url: string;
 }
 
 type Props = {
-  books_table: Array<Book>
-}
+  books_table: Array<Book>;
+};
 
-const BookIndex= ()=> {
-  const {data} = useQuery<Props>(GET_BOOKS)
-  console.log(data)
-  const books= !data? []:data.books_table
-  
+const BookIndex = () => {
+  const { data } = useQuery<Props>(GET_BOOKS);
+  console.log(data);
+  const books = !data ? [] : data.books_table;
+
   return (
     <>
-    <div>
-      <GlobalHeader/>
-    </div>
-    <Grid container spacing={4} mt={0} px={4} >
-    {books.map((book) => (
-      // <div className='book-card-index'>
-      <Grid item xs={12} sm={6} md={4} justify-content="center">
-         <Card sx={{ maxWidth: 350 }}>
-          <CardActionArea href={`/book/${book.id}`}>
-            <CardMedia
-            component="img"
-            height="500"
-            image={book.image_url}
-            alt="本の表紙"/>
-            <CardContent>
-              <Typography gutterBottom variant="h6" component="div">
-                {book.title}
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                {book.author}
-              </Typography>
-        </CardContent>
-        </CardActionArea>
-        </Card> 
+      <div>
+        <GlobalHeader />
+      </div>
+      <Grid container spacing={4} mt={0} px={4}>
+        {books.map((book) => (
+          <Grid
+            item
+            xs={12}
+            sm={6}
+            md={4}
+            justify-content="center"
+            key={book.id}
+          >
+            <Card sx={{ maxWidth: 350 }}>
+              <CardActionArea href={`/book/${book.id}`}>
+                <CardMedia
+                  component="img"
+                  height="500"
+                  image={book.image_url}
+                  alt="本の表紙"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h6" component="div">
+                    {book.title}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    {book.author}
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          </Grid>
+          // </div>
+        ))}
       </Grid>
-        // </div>
-    ))}
-    </Grid>
-    {/* <Container maxWidth="sm"> */}
-    <div className='index-add-btn'>
-      <Button variant="contained" href="/book/book-new">REGISTER</Button>
-    </div>
-    {/* </Container> */}
+      {/* <Container maxWidth="sm"> */}
+      <div className="index-add-btn">
+        <Button variant="contained" href="/book/book-new">
+          REGISTER
+        </Button>
+      </div>
+      {/* </Container> */}
     </>
-    )
-}
+  );
+};
 
-export default BookIndex
-
-
-
+export default BookIndex;
