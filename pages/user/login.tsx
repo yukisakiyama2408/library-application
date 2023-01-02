25d773c64b95628e1391ea44e9515b4d459096b0
Yuki Sakiyama <yuki.king.0422@gmail.com>
2023-01-02 15:28:35 +0900

    indexページを更新

--- pages/user/login.tsx
+++ pages/user/login.tsx
@@ -1,20 +1,22 @@
-import { useAuth0 } from "@auth0/auth0-react"
+import { useAuth0 } from "@auth0/auth0-react";
 
+const Login = () => {
+  const { loginWithRedirect, logout, isAuthenticated } = useAuth0();
+  const handleLogout = () => {
+    logout();
+  };
 
-const Login=()=>{
-    const{loginWithRedirect,logout,isAuthenticated}=useAuth0()
-    const handleLogout =()=>{
-        logout()
-    }
-    return(
+  return (
     <>
-    <div>
-        {isAuthenticated ?
-            <button onClick={handleLogout}>ログアウト</button>
-            :  <button onClick={loginWithRedirect}>ログイン</button>
-        }
-    </div>
-    </>)
-}
+      <div>
+        {isAuthenticated ? (
+          <button onClick={handleLogout}>ログアウト</button>
+        ) : (
+          <button onClick={loginWithRedirect}>ログイン</button>
+        )}
+      </div>
+    </>
+  );
+};
 
-export default Login
+export default Login;
