4fae7bb982088035b20ccd3cfc9d8f9a4f2b4b3c
Yuki Sakiyama <yuki.king.0422@gmail.com>
2023-01-08 18:15:22 +0900

    Google Books APIを用いた検索機能を追加

---
+++ pages/book/search/booksSearch.tsx
@@ -0,0 +1,8 @@
+import useSearch from "./useSearch";
+
+const BooksSearch = () => {
+  const { handleNewBooks, searchBooks, value, items } = useSearch();
+  return <>aaa</>;
+};
+
+export default BooksSearch;
