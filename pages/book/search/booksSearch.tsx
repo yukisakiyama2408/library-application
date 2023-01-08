2eff71df508b2704fb859ae4464abdfe890527f3
Yuki Sakiyama <yuki.king.0422@gmail.com>
2023-01-08 18:20:54 +0900

    検索用のコンポーネントにmaterial UIを追加

--- pages/book/search/booksSearch.tsx
+++ pages/book/search/booksSearch.tsx
@@ -1,8 +1,23 @@
 import useSearch from "./useSearch";
+import { TextField } from "@mui/material";
 
 const BooksSearch = () => {
   const { handleNewBooks, searchBooks, value, items } = useSearch();
-  return <>aaa</>;
+  return (
+    <>
+      <div>
+        <form onSubmit={searchBooks}>
+          <TextField
+            onChange={handleNewBooks}
+            value={value}
+            id="outlined-basic"
+            label="Outlined"
+            variant="outlined"
+          />
+        </form>
+      </div>
+    </>
+  );
 };
 
 export default BooksSearch;
