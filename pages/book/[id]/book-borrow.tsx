8a09bc44e42553ad658a85f539d2d86575f7648a
Yuki Sakiyama <yuki.king.0422@gmail.com>
2023-01-11 18:43:30 +0900

    本貸出処理の箇所を修正

--- pages/book/[id]/book-borrow.tsx
+++ pages/book/[id]/book-borrow.tsx
@@ -1,5 +1,6 @@
-import { gql, useMutation } from "@apollo/client";
+import { gql, useMutation, useQuery } from "@apollo/client";
 import { useState } from "react";
+import { useAuth0 } from "@auth0/auth0-react";
 import {
   Button,
   Dialog,
@@ -10,8 +11,13 @@ import {
 } from "@mui/material";
 
 const BORROW_BOOK = gql`
-  mutation ($borrowed_book_id: Int!) {
-    insert_borrowed_books(objects: { borrowed_book_id: $borrowed_book_id }) {
+  mutation ($borrowed_book_id: Int!, $borrowing_user_id: Int!) {
+    insert_borrowed_books(
+      objects: {
+        borrowed_book_id: $borrowed_book_id
+        borrowing_user_id: $borrowing_user_id
+      }
+    ) {
       affected_rows
       returning {
         id
@@ -23,17 +29,41 @@ const BORROW_BOOK = gql`
   }
 `;
 
+const GET_USER = gql`
+  query getUser($authId: String!) {
+    users_table(where: { authId: { _eq: $authId } }) {
+      id
+      name
+      company
+    }
+  }
+`;
+
 type BookId = {
   id: number;
 };
 
 const BookBorrow: React.FC<BookId> = ({ id }) => {
+  const { user } = useAuth0();
+  console.log(user);
+  if (user) {
+    console.log(user.sub);
+  }
+
+  const { data } = useQuery(GET_USER, {
+    variables: { authId: user && user.sub },
+  });
+
+  console.log(data);
+  const user_info = !data ? [] : data.users_table[0];
+  console.log(user_info);
+
   const [borrowBook] = useMutation(BORROW_BOOK, {
     refetchQueries: ["GetBooks"],
   });
   const handleBorrowBook = () => {
     borrowBook({
-      variables: { borrowed_book_id: id },
+      variables: { borrowed_book_id: id, borrowing_user_id: user_info.id },
     });
   };
 
