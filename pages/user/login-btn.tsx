5d4267987f907e8e84180da2bc6eee68089c4409
Yuki Sakiyama <yuki.king.0422@gmail.com>
2023-02-26 16:50:36 +0900

    fix

--- pages/user/login-btn.tsx
+++ pages/user/login-btn.tsx
@@ -1,21 +1,21 @@
-import { useSession, signIn, signOut } from "next-auth/react";
+// import { useSession, signIn, signOut } from "next-auth/react";
 
-const LoginBtn = () => {
-  const { data: session } = useSession();
-  if (session) {
-    console.log(session);
-    return (
-      <>
-        Signed in as {session.user && session.user.email} <br />{" "}
-        <button onClick={() => signOut()}>Sign out</button>{" "}
-      </>
-    );
-  }
-  return (
-    <>
-      Not signed in <br /> <button onClick={() => signIn()}>Sign in</button>{" "}
-    </>
-  );
-};
+// const LoginBtn = () => {
+//   const { data: session } = useSession();
+//   if (session) {
+//     console.log(session);
+//     return (
+//       <>
+//         Signed in as {session.user && session.user.email} <br />{" "}
+//         <button onClick={() => signOut()}>Sign out</button>{" "}
+//       </>
+//     );
+//   }
+//   return (
+//     <>
+//       Not signed in <br /> <button onClick={() => signIn()}>Sign in</button>{" "}
+//     </>
+//   );
+// };
 
-export default LoginBtn;
+// export default LoginBtn;
